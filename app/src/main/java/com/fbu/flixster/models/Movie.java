8f9ebb1fa8507c45545d19624b0ce5475e895c14
Matt Davison <mdavison@fb.com>
2020-06-24 14:28:42 -0400

    Added Movie Model

---
+++ app/src/main/java/com/fbu/flixster/models/Movie.java
@@ -0,0 +1,49 @@
+package com.fbu.flixster.models;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    public static final String TAG = "Movie";
+
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) {
+        try {
+            posterPath = jsonObject.getString("poster_path");
+            title = jsonObject.getString("title");
+            overview = jsonObject.getString("overview");
+        } catch (JSONException e) {
+            Log.e(TAG, "results not found in json", e);
+        }
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
